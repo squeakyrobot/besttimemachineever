6ce4da6186a2976598c1f296610c975ba39e13fe
squeakyrobot <ryan@squeakyrobot.com>
2023-09-02 14:37:38 -0700

    Added testimonials

---
+++ src/lib/components/Testimonials.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	//
+</script>
+
+<div class="carousel w-full h-40 md:h-80">
+	<div id="item1" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/sheryl.png" class="object-contain" />
+	</div>
+	<div id="item2" class="carousel-item w-full">
+		<img src="/images/testimonials/sam.png" class="object-contain" />
+	</div>
+	<div id="item3" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/willis.png" class="object-contain" />
+	</div>
+	<div id="item4" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/navin.png" class="object-contain" />
+	</div>
+	<div id="item5" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/luna.png" class="object-contain" />
+	</div>
+	<div id="item6" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/gramps.png" class="object-contain" />
+	</div>
+	<div id="item7" class="carousel-item w-full justify-center">
+		<img src="/images/testimonials/elvis.png" class="object-contain" />
+	</div>
+</div>
+<div class="flex justify-center w-full py-2 gap-2">
+	<a href="#item1" class="btn btn-xs">1</a>
+	<a href="#item2" class="btn btn-xs">2</a>
+	<a href="#item3" class="btn btn-xs">3</a>
+	<a href="#item4" class="btn btn-xs">4</a>
+	<a href="#item5" class="btn btn-xs">5</a>
+	<a href="#item6" class="btn btn-xs">6</a>
+	<a href="#item7" class="btn btn-xs">7</a>
+</div>
