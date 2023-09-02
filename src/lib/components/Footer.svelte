29f7413037bacdb44ae31d8b28186cbab142eb6c
squeakyrobot <ryan@squeakyrobot.com>
2023-09-01 19:55:50 -0700

    Adjusted the css height calc

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -19,18 +19,19 @@
 	<div class="align-bottom">
 		<a
 			href="/"
-			class="items-center inline-flex"
+			class="items-center inline-flex p2"
 			title="BestTimeMachineEver.com"
 			data-sveltekit-reload
 		>
-			<div class="hidden sm:block link link-hover p2">
+			<div class="hidden sm:block link link-hover p-2">
 				BestTimeMachineEver<em>.com</em>
 			</div>
 			<div class="p-2 sm:hidden link link-hover">Home</div>
 		</a>
 	</div>
 	<div class="flex">
-		<a href="/classic/TimeMachine.html" class="link link-hover p-2">Classic</a>
+		<!-- <a href="/classic/TimeMachine.html" class="link link-hover p-2">Classic</a> -->
+		<a href="/white-paper.pdf" target="_blank" class="link link-hover p-2">Research</a>
 		<a href="/about" class="link link-hover p-2">About</a>
 		<a href="/terms-of-service" class="link link-hover p-2">Terms</a>
 		<a href="/privacy-policy" class="link link-hover p-2">Privacy</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="grid place-items-center min-h-screen bg-no-repeat bg-cover w-full"
+	class="grid place-items-center h-[calc(100dvh)] bg-no-repeat bg-cover w-full"
 	style="background-image: url(/images/space-bg-3.jpg);"
 >
 	<slot />
